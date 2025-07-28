d4254d446d30ce31782cd40a739f646877665d8b
Roberto Junior <juniorspinola16@gmail.com>
2025-07-28 01:01:13 -0300

    logica e configurações de algumas activities

--- app/src/main/java/com/robertojr/ui/CriarContaActivity1.kt
+++ app/src/main/java/com/robertojr/ui/CriarContaActivity1.kt
@@ -1,10 +1,12 @@
 package com.robertojr.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import com.robertojr.moov.R
 import com.robertojr.moov.databinding.ActivityCriarConta1Binding
-import kotlinx.coroutines.launch
 
 class CriarContaActivity1 : AppCompatActivity() {
     lateinit var binding: ActivityCriarConta1Binding
@@ -15,9 +17,60 @@ class CriarContaActivity1 : AppCompatActivity() {
         supportActionBar?.hide()
 
 
-        lifecycleScope.launch {
+
+        val watcher = {
+            var nome = binding.edtNomeCadastro.text.toString()
+            var sobrenome = binding.edtSobrenome.text.toString()
+            var phone = binding.edtPhone.text.toString()
+            var dataNascimento = binding.edtDataNascimento.text.toString()
+
+            val regexNome = nome.isNotEmpty()
+            val regexSobrenome = sobrenome.isNotEmpty()
+            val regexPhone = phone.isNotEmpty()
+            val regexDataNascimento = dataNascimento.isNotEmpty()
+
+            val validate = regexNome and regexSobrenome and regexPhone and regexDataNascimento
+            binding.btnCriarContaAvancar.isEnabled = validate
+            binding.btnCriarContaAvancar.backgroundTintList =
+                ContextCompat.getColorStateList(
+                    this@CriarContaActivity1,
+                    if (validate) R.color.amarelo_principal else R.color.btn_desativado
+                )
+
+            if (!regexNome) binding.edtNomeCadastro.error = getString(R.string.campoIncompleto)
+            if (!regexSobrenome) binding.edtSobrenome.error = getString(R.string.campoIncompleto)
+            if (!regexPhone) binding.edtPhone.error = getString(R.string.campoIncompleto)
+            if (!regexDataNascimento) binding.edtDataNascimento.error = getString(R.string.campoIncompleto)
+
+        }
+
+        binding.edtNomeCadastro.addTextChangedListener { watcher() }
+        binding.edtSobrenome.addTextChangedListener { watcher() }
+        binding.edtPhone.addTextChangedListener { watcher() }
+        binding.edtDataNascimento.addTextChangedListener { watcher() }
+
+
+        binding.btnCriarContaAvancar.setOnClickListener {
+            val intent = Intent(this@CriarContaActivity1, CriarContaActivity2::class.java)
+
+
+            var nome = binding.edtNomeCadastro.text.toString()
+            var sobrenome = binding.edtSobrenome.text.toString()
+            var phone = binding.edtPhone.text.toString()
+            var dataNascimento = binding.edtDataNascimento.text.toString()
+
+
+            intent.putExtra("nome", nome)
+            intent.putExtra("sobrenome", sobrenome)
+            intent.putExtra("phone", phone)
+            intent.putExtra("dataNascimento", dataNascimento)
+
+            startActivity(intent)
 
 
         }
+
+
     }
+
 }
--- app/src/main/java/com/robertojr/ui/CriarContaActivity2.kt
+++ app/src/main/java/com/robertojr/ui/CriarContaActivity2.kt
@@ -1,21 +1,105 @@
 package com.robertojr.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
 import com.robertojr.moov.R
+import com.robertojr.moov.databinding.ActivityCriarConta2Binding
+import kotlinx.coroutines.launch
 
 class CriarContaActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityCriarConta2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_criar_conta2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        supportActionBar?.hide()
+        binding = ActivityCriarConta2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nome = intent.getStringExtra("nome")
+        val sobrenome = intent.getStringExtra("sobrenome")
+        val phone = intent.getStringExtra("phone")
+        val dataNascimento = intent.getStringExtra("dataNascimento")
+
+        val watcher = {
+            var usuario = binding.edtUsuarioCadastro.text.toString()
+            var senha = binding.edtSenhaCadastro.text.toString()
+            var senhaConfirma = binding.edtSenhaCadastroConfirmar.text.toString()
+            var email = binding.edtEmail.text.toString()
+
+            val regexUsuario = usuario.isNotEmpty()
+            val regexSenha = senha.isNotEmpty()
+            val regexSenhaConfirma = senhaConfirma.isNotEmpty()
+            val regexEmail = email.isNotEmpty()
+
+            val validate = regexUsuario and regexSenha and regexSenhaConfirma and regexEmail
+            binding.btnCriarContaAvancar.isEnabled = validate
+            binding.btnCriarContaAvancar.backgroundTintList =
+                ContextCompat.getColorStateList(
+                    this@CriarContaActivity2,
+                    if (validate) R.color.amarelo_principal else R.color.btn_desativado
+                )
+
+            if (!regexUsuario) binding.edtUsuarioCadastro.error =
+                getString(R.string.campoIncompleto)
+            if (!regexSenha) binding.edtSenhaCadastro.error = getString(R.string.campoIncompleto)
+            if (!regexSenhaConfirma) binding.edtSenhaCadastroConfirmar.error =
+                getString(R.string.campoIncompleto)
+            if (!regexEmail) binding.edtEmail.error = getString(R.string.campoIncompleto)
+
         }
+
+        binding.edtUsuarioCadastro.addTextChangedListener { watcher() }
+        binding.edtSenhaCadastro.addTextChangedListener { watcher() }
+        binding.edtSenhaCadastroConfirmar.addTextChangedListener { watcher() }
+        binding.edtEmail.addTextChangedListener { watcher() }
+
+
+
+        binding.btnCriarContaAvancar.setOnClickListener {
+            lifecycleScope.launch {
+                val intent = Intent(this@CriarContaActivity2, CriarContaActivity3::class.java)
+
+                var usuario = binding.edtUsuarioCadastro.text.toString()
+                var senha = binding.edtSenhaCadastro.text.toString()
+                var email = binding.edtEmail.text.toString()
+
+//                try {
+//
+//                    var userSenha = Login(email,null,senha,null,usuario)
+//                    var valida = RetrofitClient.loginRetrofit.validateLogin(userSenha)
+//
+//                    if (valida.isSuccessful){
+//
+//                    }else{
+//
+//                    }
+//
+//                }catch (e: Exception){
+//                    e.printStackTrace()
+//                }
+
+
+
+
+                intent.putExtra("nome", nome)
+                intent.putExtra("sobrenome", sobrenome)
+                intent.putExtra("phone", phone)
+                intent.putExtra("dataNascimento", dataNascimento)
+                intent.putExtra("usuario", usuario)
+                intent.putExtra("senha", senha)
+                intent.putExtra("email", email)
+
+
+                startActivity(intent)
+
+
+            }
+
+
+        }
+
     }
 }
